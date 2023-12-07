63f05397449f9d45cfe2a8428b87551394b608c0
Doug Hammond <d.lakehammond@gmail.com>
2023-12-07 22:16:11 +0100

    Day 7 part 2

--- day_7.ts
+++ day_7.ts
@@ -6,6 +6,7 @@ interface Hand {
   cards: string;
   bid: number;
   ordinal: string;
+  jokerOrdinal: string;
 }
 
 function cardFreqs(cards: string): { [card: string]: number } {
@@ -17,19 +18,19 @@ function cardFreqs(cards: string): { [card: string]: number } {
 }
 
 const CARD_VALUES: { [card: string]: string } = {
-  '2': 'a',
-  '3': 'b',
-  '4': 'c',
-  '5': 'd',
-  '6': 'e',
-  '7': 'f',
-  '8': 'g',
-  '9': 'h',
-  'T': 'i',
-  'J': 'j',
-  'Q': 'k',
-  'K': 'l',
-  'A': 'm'
+  '2': 'b',
+  '3': 'c',
+  '4': 'd',
+  '5': 'e',
+  '6': 'f',
+  '7': 'g',
+  '8': 'h',
+  '9': 'i',
+  'T': 'j',
+  'J': 'k',
+  'Q': 'l',
+  'K': 'm',
+  'A': 'n'
 };
 // Convert hands into strings that can be sorted by string order
 function handOrdinal(cards: string): string {
@@ -40,20 +41,42 @@ function handOrdinal(cards: string): string {
   return `${handValue}: ${cardValues}`;
 }
 
+const JOKER_CARD_VALUES = { ...CARD_VALUES };
+JOKER_CARD_VALUES.J = 'a';
+function jokerHandOrdinal(cards: string): string {
+  const cardValues = cards.replace(/./g, c => JOKER_CARD_VALUES[c]);
+
+  const freqs = cardFreqs(cards);
+  const jokers = freqs.J || 0;
+  delete freqs.J;
+
+  const values = Object.values(freqs).sort().reverse();
+  values[0] = (values[0] || 0) + jokers;
+
+  return `${values.join('')}: ${cardValues}`;
+}
+
 function parseHand(line: string): Hand {
   const [cards, bid] = line.split(' ');
 
   return {
     cards,
     bid: parseInt(bid),
-    ordinal: handOrdinal(cards)
+    ordinal: handOrdinal(cards),
+    jokerOrdinal: jokerHandOrdinal(cards)
   };
 }
 
+function totalWinnings(hands: Hand[]) {
+  return hands.map(({ bid }, i) => bid * (i + 1)).reduce((acc, winnings) => acc + winnings);
+}
+
 const hands = input.map(parseHand);
-hands.sort(({ ordinal: a }, { ordinal: b }) => a.localeCompare(b));
 
 console.log('Part 1:');
-console.log(
-  hands.map(({ bid }, i) => bid * (i + 1)).reduce((acc, winnings) => acc + winnings)
-);
+hands.sort(({ ordinal: a }, { ordinal: b }) => a.localeCompare(b));
+console.log(totalWinnings(hands));
+
+console.log('Part 2:');
+hands.sort(({ jokerOrdinal: a }, { jokerOrdinal: b }) => a.localeCompare(b));
+console.log(totalWinnings(hands));
