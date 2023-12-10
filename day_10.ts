0b431c1e2ec614c291136a4fe306581b6392e5fc
Doug Hammond <d.lakehammond@gmail.com>
2023-12-10 22:25:54 +0100

    Day 10 part 2

--- day_10.ts
+++ day_10.ts
@@ -37,6 +37,11 @@ class Shape {
     if (dir.isEqual(this.a)) return this.b;
     if (dir.isEqual(this.b)) return this.a;
   }
+
+  isEqual(other: Shape): boolean {
+    return (this.a.isEqual(other.a) && this.b.isEqual(other.b)) ||
+           (this.a.isEqual(other.b) && this.b.isEqual(other.a));
+  }
 }
 
 const NORTH = new Position( 0, -1);
@@ -44,13 +49,20 @@ const SOUTH = new Position( 0,  1);
 const EAST  = new Position( 1,  0);
 const WEST  = new Position(-1,  0);
 
+const NORTH_SOUTH = new Shape(NORTH, SOUTH);
+const EAST_WEST = new Shape(EAST, WEST);
+const NORTH_WEST = new Shape(NORTH, WEST);
+const NORTH_EAST = new Shape(NORTH, EAST);
+const SOUTH_WEST = new Shape(SOUTH, WEST);
+const SOUTH_EAST = new Shape(SOUTH, EAST);
+
 const SHAPES: { [chr: string]: Shape } = {
-  '-': new Shape( EAST,  WEST),
-  '|': new Shape(NORTH, SOUTH),
-  'J': new Shape(NORTH,  WEST),
-  '7': new Shape(SOUTH,  WEST),
-  'L': new Shape(NORTH,  EAST),
-  'F': new Shape(SOUTH,  EAST),
+  '-': EAST_WEST,
+  '|': NORTH_SOUTH,
+  'J': NORTH_WEST,
+  '7': SOUTH_WEST,
+  'L': NORTH_EAST,
+  'F': SOUTH_EAST,
 };
 
 class Pipe {
@@ -111,34 +123,88 @@ function startPipe(map: PipeMap, pos: Position) {
   return new Pipe(pos, shape);
 }
 
-function farthestDistance(map: PipeMap, start: Pipe): number {
-  const at = ({ x, y }: Position): Pipe => {
-    const pipe = map[y][x];
-    if (!pipe) throw 'Not found';
-    return pipe;
-  };
+class Loop {
+  map: PipeMap;
+  length: number;
 
-  let prevA = start;
-  let prevB = start;
-  let a: Pipe = at(start.pos.plus(start.shape.a));
-  let b: Pipe = at(start.pos.plus(start.shape.b));
-  let distance: number = 1;
+  constructor(map: PipeMap, start: Pipe) {
+    this.map = [];
+    this.length = 2;
 
-  while (a !== b) {
-    const nextA = at(a.opposite(prevA.pos));
-    const nextB = at(b.opposite(prevB.pos));
-    prevA = a;
-    prevB = b;
-    a = nextA;
-    b = nextB;
-    distance++;
+    const at = ({ x, y }: Position): Pipe => {
+      const pipe = map[y][x];
+      if (!pipe) throw 'Not found';
+      return pipe;
+    };
+
+    let prevA = start;
+    let prevB = start;
+    let a: Pipe = at(start.pos.plus(start.shape.a));
+    let b: Pipe = at(start.pos.plus(start.shape.b));
+
+    this.add(start);
+    this.add(a);
+    this.add(b);
+
+    while (a !== b) {
+      const nextA = at(a.opposite(prevA.pos));
+      const nextB = at(b.opposite(prevB.pos));
+      prevA = a;
+      prevB = b;
+      a = nextA;
+      b = nextB;
+
+      this.add(a);
+      this.add(b);
+      this.length += 2;
+    }
   }
 
-  return distance;
-}
+  add(pipe: Pipe) {
+    const { pos: { x, y } } = pipe;
+    this.map[y] ||= [];
+    this.map[y][x] = pipe;
+  }
+
+  contained(): number {
+    let count = 0;
+
+    for (const row of this.map) {
+      let inside = false;
+      let fromNorth: boolean;
+      for (let x = 0; x < row.length; ++x) {
+        if (!row[x]) {
+          if (inside) count++;
+          continue;
+        }
+
+        const { shape } = row[x];
+        if (shape.isEqual(NORTH_SOUTH))
+          inside = !inside;
+
+        if (shape.isEqual(NORTH_EAST))
+          fromNorth = true;
+
+        if (shape.isEqual(SOUTH_EAST))
+          fromNorth = false;
+
+        if (shape.isEqual(SOUTH_WEST) && fromNorth)
+          inside = !inside;
+
+        if (shape.isEqual(NORTH_WEST) && !fromNorth)
+          inside = !inside;
+      }
+    }
+
+    return count;
+  }
+};
 
 const map: PipeMap = parseMap(input);
 const start = startPipe(map, findStart(input));
+const loop = new Loop(map, start);
 
 console.log('Part 1:');
-console.log(farthestDistance(map, start));
+console.log(loop.length / 2);
+console.log('Part 2:');
+console.log(loop.contained());
