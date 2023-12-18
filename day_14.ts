5f5cb3f4e74084942b7906e0b54ff4e1ff471884
Doug Hammond <d.lakehammond@gmail.com>
2023-12-18 23:18:40 +0100

    Day 14 part 2

--- day_14.ts
+++ day_14.ts
@@ -2,36 +2,6 @@ import { readInputLines } from './input/read';
 
 const grid = readInputLines(14);
 
-function transposeGrid(grid: string[]): string[] {
-  const columns: string[] = [];
-
-  for (let i = 0; i < grid[0].length; ++i)
-    columns.push(grid.map(line => line[i]).join(''));
-
-  return columns;
-}
-
-const columns = transposeGrid(grid);
-
-function rowLoad(row: string): number {
-  let load = 0;
-  let weight = row.length;
-
-  for (let i = 0; i < row.length; ++i) {
-    const c = row[i];
-    if (c === 'O')
-      load += weight--;
-
-    if (c === '#')
-      weight = row.length - i - 1;
-  }
-
-  return load;
-}
-
-console.log('Part 1:');
-console.log(columns.map(rowLoad).reduce((acc, n) => acc + n));
-
 enum Rock {
   None,
   Sphere,
@@ -61,8 +31,9 @@ function tiltRow(row: Rock[]) {
     }
   }
 }
-function tilt(rocks: Rock[][]) {
+function tilt(rocks: Rock[][]): Rock[][] {
   for (const row of rocks) tiltRow(row);
+  return rocks;
 }
 function rotate(rocks: Rock[][]): Rock[][] {
   const height = rocks.length;
@@ -79,7 +50,47 @@ function load(rocks: Rock[][]): number {
               .reduce((acc, n) => acc + n);
 }
 
+function key(rocks: Rock[][]): string {
+  let key: bigint = BigInt(0);
+
+  for (const row of rocks)
+    for (const rock of row) {
+      key = (key << BigInt(1)) + BigInt(rock === Rock.Sphere);
+    }
+
+  return key.toString(36);
+}
+
 console.log('Part 1:');
 const tilted = rotate(rocks);
 tilt(tilted);
 console.log(load(tilted));
+
+function findLoop(rocks: Rock[][]) {
+  const cycle: { [key: string]: number} = {};
+  const loads: number[] = [];
+  let step = 0;
+  rocks = tilt(rotate(rocks));
+  let k = key(rocks);
+  while (!cycle[k]) {
+    cycle[k] = step++;
+    for (let i = 0; i < 4; ++i) {
+      rocks = rotate(rocks);
+      if (i === 3)
+        loads.push(load(rocks));
+      tilt(rocks);
+    }
+    k = key(rocks);
+  }
+
+  const start = cycle[k];
+  const end = step;
+  const period = end - start;
+  const i = ((1000000000 - start) % period) + start - 1;
+  console.log(start, end, period, i);
+  console.log(loads);
+  return loads[i];
+}
+
+console.log('Part 2:');
+console.log(findLoop(rocks));
