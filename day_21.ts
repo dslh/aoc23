8c502535b468245d52f09827e5e6290ed3fdbd69
Doug Hammond <d.lakehammond@gmail.com>
2023-12-22 11:54:49 +0100

    Day 21 part 1

---
+++ day_21.ts
@@ -0,0 +1,97 @@
+import { readInputLines } from './input/read';
+import { Position, adjacent } from './lib/pos';
+import Queue from './lib/queue';
+import { Grid, createGrid } from './lib/grid';
+
+const input: string[] = readInputLines(21);
+
+const isRock = ({ x, y }: Position): boolean => input[y][x] === '#';
+
+function findStart(input: string[]): Position {
+  const y = input.findIndex(line => line.includes('S'));
+  const x = input[y].indexOf('S');
+
+  return { x, y };
+}
+
+const start = findStart(input);
+
+interface Visit {
+  pos: Position;
+  steps: number;
+}
+
+const countEvenSteps = (maxSteps: number): number => {
+  const queue: Queue<Visit> = new Queue<Visit>();
+  queue.push({ pos: start, steps: 0 });
+  const visited: Grid<boolean> = createGrid<boolean>();
+
+  let evens = 0;
+
+  while (!queue.empty) {
+    const { pos, steps }: Visit = queue.pop();
+    if (visited.getAt(pos)) continue;
+    visited.setAt(pos, true);
+
+    if ((steps & 1) === 0) evens++;
+    if (steps === maxSteps) continue;
+
+    for (const next of adjacent(pos)) {
+      if (isRock(next)) continue;
+      queue.push({ pos: next, steps: steps + 1 });
+    }
+  }
+
+  return evens;
+}
+
+console.log('Part 1:');
+console.log(countEvenSteps(64));
+
+console.log('Part 2:');
+console.log(input[0].length, input.length);
+// 26501365
+// 5 11 481843
+function sumUpToN(n: number): number {
+  return n * (n + 1) / 2;
+}
+function manhattanArea(radius: number) {
+  return sumUpToN(radius) + sumUpToN(radius - 1);
+}
+
+/**
+ * o
+ * = 1
+ *
+ *  o
+ * ooo
+ *  o
+ * = 5 (+4)
+ *
+ *   o
+ *  ooo
+ * ooooo
+ *  ooo
+ *   o
+ * = 13 (+8)
+ *
+ *    o
+ *   ooo
+ *  ooooo
+ * ooooooo
+ *  ooooo
+ *   ooo
+ *    o
+ * = 25 (+12)
+ *
+ *     o
+ *    ooo
+ *   ooooo
+ *  ooooooo
+ * ooooooooo
+ *  ooooooo
+ *   ooooo
+ *    ooo
+ *     o
+ * = 41 (+16)
+ */
