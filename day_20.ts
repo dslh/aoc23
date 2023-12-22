13ce3f12849af639e750dae7fb62b2f3253b31d8
Doug Hammond <d.lakehammond@gmail.com>
2023-12-23 00:12:43 +0100

    Day 20 part 2

--- day_20.ts
+++ day_20.ts
@@ -1,7 +1,7 @@
 import { readInputLines } from './input/read';
 import Queue from './lib/queue';
 
-const input: string[] = readInputLines(20);
+const input: string[] = readInputLines(20).filter(line => ['b', '%', '&'].includes(line[0]));
 
 interface Pulse {
   src: string;
@@ -15,13 +15,15 @@ interface Module {
 
   attach: (input: string) => void;
   receive: (pulse: Pulse) => boolean | undefined;
+  toString: () => string;
 }
 
 function Broadcaster(name: string, outputs: string[]): Module {
   return {
     name, outputs,
     attach: () => {},
-    receive: ({ high }: Pulse) => high
+    receive: ({ high }: Pulse) => high,
+    toString: () => 'broadcaster'
   };
 }
 
@@ -36,7 +38,8 @@ function FlipFlop(name: string, outputs: string[]): Module {
 
       state = !state;
       return state;
-    }
+    },
+    toString: () => `%${name}${state ? '+' : '-'}`
   };
 }
 
@@ -49,7 +52,10 @@ function Conjunction(name: string, outputs: string[]): Module {
     receive: ({ src, high }: Pulse) => {
       memory[src] = high;
       return !Object.values(memory).every(high => high);
-    }
+    },
+    toString: () => `&${name} (${
+      Object.keys(memory).map(input => `${input}${memory[input] ? '+' : '-'}`).join(' ')
+    })`
   };
 }
 
@@ -134,6 +140,24 @@ for (let i = 0; i < 1000; i++)
   network.pushButton();
 console.log(network.highs * network.lows);
 
+/**
+ * The broadcast module sends to four flip-flops.
+ * Each one is the entry point to an isolated subsystem.
+ * Each subsystem has a single output, a conjunction module
+ * that itself has only one output and only one input.
+ * All four subsystems output to a conjunction module, and
+ * this conjunction module outputs to rx, the target.
+ *
+ * Each subsystem is set up to output a high value once every
+ * n button pushes, where the n-values for all subsystems are
+ * coprime.
+ *
+ * To solve part two, remove the output conjunction module
+ * from three of the four subsystems and run the code below,
+ * noting the value of rxNetwork.pushes. Repeat this for the
+ * other three subsystems, then multiply the four values to
+ * get the answer for part 2.
+ */
 console.log('Part 2:');
 const rxNetwork = new Network(input.map(parseModule));
 rxNetwork.awaitRx();
